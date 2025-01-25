0be5695126d639e2c6566fc74772306754b3e6b9
Brian Wu <57295844+tupleu@users.noreply.github.com>
2025-01-24 19:42:40 -0500

    added test sprite

--- bubble/src/main.rs
+++ bubble/src/main.rs
@@ -1,5 +1,31 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowResolution};
+
+const WIDTH: f32 = 1920.;
+const HEIGHT: f32 = 1080.;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "bubble".to_string(),
+                        resolution: WindowResolution::new(WIDTH, HEIGHT),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Sprite::from_image(asset_server.load("test.png")),
+        Transform::from_xyz(8., 8., 2.),
+    ));
 }
